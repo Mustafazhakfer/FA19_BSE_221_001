2eae670816d2cbf035faf19a06cdbd4686861976
Mustafa Zhakfar <zhakfer@gmail.com>
2022-04-12 10:24:06 -0700

    New Assignment 001

--- app/src/main/java/com/example/fa19_bse_221_001/addRecord.java
+++ app/src/main/java/com/example/fa19_bse_221_001/addRecord.java
@@ -1,5 +1,6 @@
 package com.example.fa19_bse_221_001;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,9 +8,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class addRecord extends Fragment {
+//    EditText name,age,address;
 
     View view;
     @Override
@@ -17,6 +21,29 @@ public class addRecord extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_add_record, container, false);
+//
+//        name=view.findViewById(R.id.name);
+//        age=view.findViewById(R.id.age);
+//        address=view.findViewById(R.id.address);
+//        saveBtn=view.findViewById(R.id.save);
+
+        EditText email= view.findViewById(R.id.email);
+        Button saveBtn = view.findViewById(R.id.save);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = email.getText().toString();
+                Bundle result = new Bundle();
+                result.putString("df1",text);
+                getParentFragmentManager().setFragmentResult("dataForm1",result);
+                email.setText("");
+            }
+        });
+
+
+
         return view;
     }
+
+
 }
--- app/src/main/java/com/example/fa19_bse_221_001/searchRecord.java
+++ app/src/main/java/com/example/fa19_bse_221_001/searchRecord.java
@@ -2,11 +2,14 @@ package com.example.fa19_bse_221_001;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class searchRecord extends Fragment {
@@ -17,6 +20,17 @@ public class searchRecord extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_search_record, container, false);
+
+        getParentFragmentManager().setFragmentResultListener("dataFrom1", this, new FragmentResultListener() {
+            @Override
+            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+                String data = result.getString("df1");
+                TextView textView = view.findViewById(R.id.dataFrom1);
+                textView.setText(data);
+
+
+            }
+        });
         return view;
     }
 }
